62cee14d1d16a0a82f81b8d5630d4f9496f703b4
KoennieTime <koenvandermarel@gmail.com>
2021-04-02 12:25:06 +0200

    Basic setup

---
+++ README.md
@@ -0,0 +1,25 @@
+# IPOSE Game demo - Doodle Jump
+
+Benodigdheden:
+
+1. [Open JavaFX](https://openjfx.io/) of [Java SDK 1.8](https://www.oracle.com/nl/java/technologies/javase/javase-jdk8-downloads.html)
+
+
+
+## Wat kan je met deze game?
+
+Het oude bekende spelletje doodle jump wat voorheen veel gespeeld werd op de iPod is nu gemaakt in JavaFX met de FXGL library. Je kan dus een namaak variant van doodle jump spelen met Java voor educationele doeleinden.
+
+
+
+## Problemen?
+
+1. Controleer met het commando ```java -version``` of je daadwerkelijk op java SDK 1.8 zit of de openJfx versie.
+2. Controleer in Intellij bij File -> Project settings -> Project of de geselecteerde SDK overeen komt met de benodigde SDK.
+3. Pas je omgevingsvariabele JAVA_HOME aan naar het pad waar de betreffende SDK is geïnstalleerd
+
+
+
+## Bronnen
+
+- [Doodle Jump in Unity](https://github.com/Brackeys/Doodle-Jump-Replica) voor de assets
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,23 +1,95 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
+import javafx.scene.input.KeyCode;
+import javafx.scene.text.Text;
 
-public class Main extends Application {
+import java.util.Map;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
+
+public class Main extends GameApplication {
+
+    private Entity player;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(480);
+        settings.setHeight(800);
+        settings.setTitle("Doodle Jump");
+        settings.setVersion("0.1");
+        settings.setIntroEnabled(false);
+        settings.setMainMenuEnabled(false);
+        settings.setGameMenuEnabled(false);
     }
 
+    @Override
+    protected void initInput() {
+        FXGL.onKey(KeyCode.D, () -> {
+            player.translateX(5); // move right 5 pixels
+            FXGL.inc("pixelsMoved", +5);
+        });
+
+        FXGL.onKey(KeyCode.A, () -> {
+            player.translateX(-5); // move left 5 pixels
+            FXGL.inc("pixelsMoved", +5);
+        });
+
+        FXGL.onKey(KeyCode.W, () -> {
+            player.translateY(-5); // move up 5 pixels
+            FXGL.inc("pixelsMoved", +5);
+        });
+
+        FXGL.onKey(KeyCode.S, () -> {
+            player.translateY(5); // move down 5 pixels
+            FXGL.inc("pixelsMoved", +5);
+        });
+    }
+
+    protected void initAssets() throws Exception {
+
+    }
+
+    @Override
+    protected void initGame() {
+        player = FXGL.entityBuilder().at(150, 150).view(texture("Doodler.png", 75, 75)).buildAndAttach();
+    }
+
+    @Override
+    protected void initPhysics() {
+
+    }
+
+    @Override
+    protected void initUI() {
+        Text textPixels = new Text();
+        textPixels.setTranslateX(50); // x = 50
+        textPixels.setTranslateY(100); // y = 100
+
+        FXGL.getGameScene().addUINode(textPixels); // add to the scene graph
+
+        textPixels.textProperty().bind(FXGL.getWorldProperties().intProperty("pixelsMoved").asString());
+
+        FXGL.getGameScene().setBackgroundRepeat("background.png");
+    }
+
+    protected void onUpdate() {
+
+    }
+
+    @Override
+    protected void initGameVars(Map<String, Object> vars) {
+        vars.put("pixelsMoved", 0);
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
+
